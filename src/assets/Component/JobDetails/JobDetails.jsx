46dcd33016da8f81bc9f694b2300d97f2cb01131
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-06 01:32:12 +0600

    updating jobDetails

--- src/assets/Component/JobDetails/JobDetails.jsx
+++ src/assets/Component/JobDetails/JobDetails.jsx
@@ -1,19 +1,24 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { HiCurrencyDollar } from "react-icons/hi";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const JobDetails = () => {
     const jobs = useLoaderData();
-    
+
     const { id } = useParams();
     const intid = parseInt(id);
     const job = jobs.find(job => job.id === intid)
     console.log(job);
+
+    const ApplyJob = () => toast("You have Apply job successfully ");
+
     return (
         <div>
             <h2>Job Details id :{id}</h2>
-            <div className="grid gap-4 md:grid-cols-4 ">
+            <div className="grid gap-4 md:grid-cols-4   ">
                 <div className="border md:col-span-3">
                     <p > <span className="font-bold">JOb Description :</span> {job.job_description}</p>
                     <p className="mt-8 "><span className="font-bold">Job Responsibility : </span>{job.job_responsibility}</p>
@@ -30,26 +35,27 @@ const JobDetails = () => {
                         <br />
                         {job.experiences}
                     </p>
-                   
+
 
                 </div>
                 <div className="border">
                     <div className="p-5">
-                    <h2 className=" text-x underline-offset-4 font-bold">Job Details</h2>
-                    <h2 ><HiCurrencyDollar className="text-2xl mr-2"></HiCurrencyDollar> <span></span></h2>
-                   
-                    
+                        <h2 className=" text-x underline-offset-4 font-bold">Job Details</h2>
+                        <h2 ><HiCurrencyDollar className="text-2xl mr-2"></HiCurrencyDollar> <span></span></h2>
 
-                    
+
+
+
                     </div>
-                    <button className="btn btn-primary">Apply Now </button>
-                   
-                   
+
+
+
 
 
                 </div>
-                
+                <button onClick={ApplyJob} className="btn btn-primary ">Apply Now </button>
             </div>
+            <ToastContainer />
         </div>
     );
 };
